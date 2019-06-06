523fda4044e58bb6b5a5d8dcf34b3ecc9b33a8b0
hill_0623 <hill_0623@qq.com>
2019-06-06 14:25:27 +0800

    f3

--- config/env/development.js
+++ config/env/development.js
@@ -1,5 +1,6 @@
 ﻿module.exports = {
-     db: 'mongodb://localhost/mean-book',
+   // db: 'mongodb://localhost/mean-book',
+    db: 'mongodb://tomatohill:95USeHXQsrgLFxFgkHoTuACoBUYovTanQ4qljT8ol860HCIp1FpaEWVs3UhFYNCj0vCKcebWy8r5Jy5lXCKs8A==@tomatohill.documents.azure.com:10255/mean-dev?ssl=true&sslverifycertificate=false',
    // db:'mongodb://admin:rosemary@127.0.0.1:27017/posts',
     sessionSecret:'xxaass11dd'
 };
