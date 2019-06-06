5959f1d58be81f77e7b60316dc4006dfa3c9a6ed
hill_0623 <hill_0623@qq.com>
2019-06-06 16:50:11 +0800

    j1

---
+++ obj/Release/Package/PackageTmp/config/env/production.js
@@ -0,0 +1,4 @@
+﻿module.exports = {
+    db: 'mongodb://tomatohill:95USeHXQsrgLFxFgkHoTuACoBUYovTanQ4qljT8ol860HCIp1FpaEWVs3UhFYNCj0vCKcebWy8r5Jy5lXCKs8A==@tomatohill.documents.azure.com:10255/mean-dev?ssl=true&sslverifycertificate=false',
+     sessionSecret:'xxaa11ss'
+};
